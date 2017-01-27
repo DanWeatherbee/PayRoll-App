ca4a07301cc319e8374fac4250a53c78608aac54
danweatherbee <dan@jedi-clan.us>
2017-01-27 04:45:45 -0800

    feat: prebuild adding functionality to app

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+/*
+Startup
+So now we have two views: AppView and TodoView.
+The former needs to be instantiated on page load so its code gets executed.
+This can be accomplished through jQuery’s ready() utility,
+which will execute a function when the DOM is loaded.
+*/
+
+
+  var app = app || {};
+  var ENTER_KEY = 13;
+
+  $(function() {
+
+    // Kick things off by creating the **App**.
+    new app.AppView();
+
+  });
